5e6f7560db2d470017729b2eb8786698999d4ee8
Mor.Ofir <morofir92@gmail.com>
2023-09-12 14:17:45 +0300

    final

--- app/src/main/java/DeviceLogViewModel.kt
+++ app/src/main/java/DeviceLogViewModel.kt
@@ -2,18 +2,25 @@ import android.app.Application
 import android.bluetooth.*
 import android.bluetooth.le.ScanResult
 import android.os.Parcelable
+import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.launch
+
+import java.util.*
 
 class DeviceLogViewModel(application: Application) : AndroidViewModel(application) {
 
+    private  val myServiceUUID = "2A38F000-59C8-492B-9358-0E4E38FB0058"
+    private val myLogsCharacteristicUUID = UUID.fromString("2a38f002-59c8-492b-9358-0e4e38fb0058")
+
     var isConnected = MutableLiveData<Boolean>()
-    val logs: MutableLiveData<List<String>> = MutableLiveData(listOf())
+    val logs = MutableLiveData<List<String>>(mutableListOf())
     var bluetoothGatt: BluetoothGatt? = null
     private val context = getApplication<Application>().applicationContext
+    private val _logsInternal = mutableListOf<String>()
+
+
 
     private val gattCallback = object : BluetoothGattCallback() {
         override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
@@ -21,6 +28,7 @@ class DeviceLogViewModel(application: Application) : AndroidViewModel(applicatio
                 BluetoothProfile.STATE_CONNECTED -> {
                     isConnected.postValue(true)
                     addLog("Device connected")
+                    gatt.discoverServices()
                 }
                 BluetoothProfile.STATE_DISCONNECTED -> {
                     isConnected.postValue(false)
@@ -28,7 +36,45 @@ class DeviceLogViewModel(application: Application) : AndroidViewModel(applicatio
                 }
             }
         }
+
+
+        override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
+            val service = gatt.getService(UUID.fromString(myServiceUUID))
+            val characteristic = service.getCharacteristic(myLogsCharacteristicUUID)
+
+            if (gatt.setCharacteristicNotification(characteristic, true)) {
+                addLog("Logs characteristic notification enabled")
+
+                val descriptor = characteristic.getDescriptor(
+                    UUID.fromString("00002902-0000-1000-8000-00805f9b34fb")
+                )
+                descriptor.value = BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE
+                gatt.writeDescriptor(descriptor)
+            }
+        }
+
+        // Handle incoming notifications
+        override fun onCharacteristicChanged(gatt: BluetoothGatt, characteristic: BluetoothGattCharacteristic) {
+            if (characteristic.uuid == myLogsCharacteristicUUID) {
+                val logData = String(characteristic.value, Charsets.UTF_8)
+                Log.d("Bluetooth", "Raw data: ${characteristic.value.joinToString(", ") { it.toString() }}")
+
+                updateLogs(logData)
+            }
+        }
+
+
+        override fun onCharacteristicRead(gatt: BluetoothGatt, characteristic: BluetoothGattCharacteristic, status: Int) {
+            if (characteristic.uuid == myLogsCharacteristicUUID) {
+                val logData = characteristic.value?.let { String(it, Charsets.UTF_8) }
+                if (logData != null) {
+                    updateLogs(logData)
+                }
+            }
+        }
     }
+
+
 
     fun addLog(message: String) {
         val currentLogs = logs.value?.toMutableList() ?: mutableListOf()
@@ -52,4 +98,30 @@ class DeviceLogViewModel(application: Application) : AndroidViewModel(applicatio
             Toast.makeText(context, "Failed to connect", Toast.LENGTH_SHORT).show()
         }
     }
+    fun updateLogs(newLog: String) {
+        val currentLogs = logs.value?.toMutableList() ?: mutableListOf()
+        _logsInternal.add(newLog)
+
+        currentLogs.add(newLog)
+        logs.postValue(currentLogs)
+
+    }
+
+     val gattServerCallback = object : BluetoothGattServerCallback() {
+        override fun onCharacteristicReadRequest(
+            device: BluetoothDevice?,
+            requestId: Int,
+            offset: Int,
+            characteristic: BluetoothGattCharacteristic?
+        ) {
+            super.onCharacteristicReadRequest(device, requestId, offset, characteristic)
+            if (characteristic?.uuid == UUID.fromString(myServiceUUID)) {
+                val logData = characteristic?.value?.toString(Charsets.UTF_8)
+                if (logData != null) {
+                   updateLogs(logData)
+                }
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/rick/companiondevicepairing/MyDeviceSelectionActivity.kt
+++ app/src/main/java/com/rick/companiondevicepairing/MyDeviceSelectionActivity.kt
@@ -1,16 +1,15 @@
-@file:Suppress("OverrideDeprecatedMigration", "OverrideDeprecatedMigration")
-
 package com.rick.companiondevicepairing
 
 import DeviceLogViewModel
+import LogAdapter
 import android.Manifest
 import android.annotation.SuppressLint
 import android.bluetooth.*
 import android.bluetooth.le.ScanFilter
-import android.bluetooth.le.ScanResult
 import android.companion.AssociationRequest
 import android.companion.BluetoothLeDeviceFilter
 import android.companion.CompanionDeviceManager
+import android.content.Context
 import android.content.Intent
 import android.content.IntentSender
 import android.content.pm.PackageManager
@@ -23,56 +22,104 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
-
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.util.*
 
 private const val SELECT_DEVICE_REQUEST_CODE = 0
 private const val REQUEST_CODE_BT_PERMISSIONS = 1001
+private const val myServiceUUID = "2A38F000-59C8-492B-9358-0E4E38FB0058"
+private  val DEVICE_LOGS_CHARACTERISTIC_ID = "2a38f002-59c8-492b-9358-0e4e38fb0058"
+
+
 
 class MyDeviceSelectionActivity : AppCompatActivity() {
 
-    companion object {
-        private val TAG = MyDeviceSelectionActivity::class.java.simpleName
-    }
+    private lateinit var viewModel: DeviceLogViewModel
+
+    lateinit var txtDeviceStatus: TextView
+    lateinit var scanButton: Button
 
     private val deviceManager: CompanionDeviceManager by lazy(LazyThreadSafetyMode.NONE) {
         getSystemService(CompanionDeviceManager::class.java)
     }
-    private lateinit var viewModel: DeviceLogViewModel
 
-
-    lateinit var txtDeviceStatus: TextView
-    lateinit var txtLogStatus: TextView
-    lateinit var scanButton: Button
-    val foundDevices: MutableLiveData<List<ScanResult>> = MutableLiveData()
-    private var bluetoothGatt: BluetoothGatt? = null
-    var isConnected = MutableLiveData<Boolean>()
-    val logs: MutableLiveData<List<String>> = MutableLiveData(listOf())
-
-
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_selection_device_my)
         viewModel = ViewModelProvider(this).get(DeviceLogViewModel::class.java)
-
         txtDeviceStatus = findViewById(R.id.txtDeviceStatus)
-        txtLogStatus = findViewById(R.id.txtLogStatus)
-
         scanButton = findViewById(R.id.btn_scan)
+
+
+        val logRecyclerView: RecyclerView = findViewById(R.id.logRecyclerView)
+        val logAdapter = LogAdapter(emptyList())  // Initialize with empty list
+        logRecyclerView.layoutManager = LinearLayoutManager(this)
+        logRecyclerView.adapter = logAdapter
+
+
+
+
+        gattService()
 
         checkAndRequestPermissions()
 
-
         scanButton.setOnClickListener {
-            if (isConnected.value == true) {
-                bluetoothGatt?.disconnect()
-                isConnected.postValue(false)
+            if (viewModel.isConnected.value == true) {
+                viewModel.disconnectGatt()
+                viewModel.isConnected.postValue(false)
             } else {
                 initiateDeviceScan()
             }
         }
+        viewModel.logs.observe(this) { logList ->
+            logAdapter.logs = logList  // Or however you want to update your logs in adapter
+            logAdapter.notifyDataSetChanged()
+        }
+
+
+
+        viewModel.isConnected.observe(this) { connected ->
+            txtDeviceStatus.text =
+                if (connected) "Device is now connected" else "Device is disconnected"
+            scanButton.text = if (connected) "Disconnect" else "Scan"
+        }
     }
+
+    private fun gattService() {
+        // Initialize your BluetoothGattServer and set the callback
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        val gattServer = bluetoothManager.openGattServer(this, viewModel.gattServerCallback)
+
+        val myServiceUUID = UUID.fromString("0000180d-0000-1000-8000-00805f9b34fb")
+        val deviceLogsCharacteristicUUID = UUID.fromString(DEVICE_LOGS_CHARACTERISTIC_ID)
+
+        // Create and add service
+        val myService = BluetoothGattService(
+            myServiceUUID,
+            BluetoothGattService.SERVICE_TYPE_PRIMARY
+        )
+
+        val deviceLogsCharacteristic = BluetoothGattCharacteristic(
+            deviceLogsCharacteristicUUID,
+            BluetoothGattCharacteristic.PROPERTY_READ or BluetoothGattCharacteristic.PROPERTY_NOTIFY,
+            BluetoothGattCharacteristic.PERMISSION_READ
+        )
+
+        // Descriptor for enabling notifications
+        val configDescriptor = BluetoothGattDescriptor(
+            UUID.fromString("00002902-0000-1000-8000-00805f9b34fb"),
+            BluetoothGattDescriptor.PERMISSION_WRITE or BluetoothGattDescriptor.PERMISSION_READ
+        )
+        deviceLogsCharacteristic.addDescriptor(configDescriptor)
+
+        myService.addCharacteristic(deviceLogsCharacteristic)
+
+        gattServer?.addService(myService)
+    }
+
 
     // Permission during runtime
     private fun checkAndRequestPermissions() {
@@ -82,23 +129,42 @@ class MyDeviceSelectionActivity : AppCompatActivity() {
         val bluetoothAdminPermission = Manifest.permission.BLUETOOTH_ADMIN
         val bluetoothConnectPermission = "android.permission.BLUETOOTH_CONNECT"
 
-        if (ContextCompat.checkSelfPermission(this, bluetoothPermission) != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                bluetoothPermission
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
             permissionsNeeded.add(bluetoothPermission)
         }
-        if (ContextCompat.checkSelfPermission(this, bluetoothAdminPermission) != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                bluetoothAdminPermission
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
             permissionsNeeded.add(bluetoothAdminPermission)
         }
-        if (Build.VERSION.SDK_INT >= 31 && ContextCompat.checkSelfPermission(this, bluetoothConnectPermission) != PackageManager.PERMISSION_GRANTED) {
+        if (Build.VERSION.SDK_INT >= 31 && ContextCompat.checkSelfPermission(
+                this,
+                bluetoothConnectPermission
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
             permissionsNeeded.add(bluetoothConnectPermission)
         }
 
-
         if (permissionsNeeded.isNotEmpty()) {
-            ActivityCompat.requestPermissions(this, permissionsNeeded.toTypedArray(), REQUEST_CODE_BT_PERMISSIONS)
+            ActivityCompat.requestPermissions(
+                this,
+                permissionsNeeded.toTypedArray(),
+                REQUEST_CODE_BT_PERMISSIONS
+            )
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         when (requestCode) {
             REQUEST_CODE_BT_PERMISSIONS -> {
                 if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
@@ -111,36 +177,7 @@ class MyDeviceSelectionActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
-
-    private val gattCallback = object : BluetoothGattCallback() {
-        @SuppressLint("SetTextI18n")
-        override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
-            when (newState) {
-                BluetoothProfile.STATE_CONNECTED -> {
-                    isConnected.postValue(true)
-                    addLog("Device connected")
-                    runOnUiThread {
-                        txtDeviceStatus.text = "Device is now connected"
-                        scanButton.text = "Disconnect"
-                    }
-
-                }
-                BluetoothProfile.STATE_DISCONNECTED -> {
-                    isConnected.postValue(false)
-                    addLog("Device Disconnected")
-                    runOnUiThread {
-                        txtDeviceStatus.text = "Device is disconnected"
-                        scanButton.text = "Scan"
-                    }
-
-                }
-            }
-        }
-
-    }
-
     private fun initiateDeviceScan() {
-        val myServiceUUID = "2A38F000-59C8-492B-9358-0E4E38FB0058"
         val scanFilter = ScanFilter.Builder()
             .setServiceUuid(ParcelUuid.fromString(myServiceUUID))
             .build()
@@ -173,26 +210,11 @@ class MyDeviceSelectionActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == SELECT_DEVICE_REQUEST_CODE && resultCode == RESULT_OK) {
-            val deviceObject = data?.getParcelableExtra<Parcelable>(CompanionDeviceManager.EXTRA_DEVICE)
-            if (deviceObject is ScanResult) {
-                val device = deviceObject.device
-                bluetoothGatt = device.connectGatt(this, false, gattCallback)
-            }
-        }
-    }
-
-    private fun addLog(message: String) {
-        val currentLogs = logs.value?.toMutableList() ?: mutableListOf()
-        currentLogs.add(message)
-        logs.postValue(currentLogs)
-        runOnUiThread {
-            txtLogStatus.text = currentLogs.joinToString("\n")
+            val deviceObject =
+                data?.getParcelableExtra<Parcelable>(CompanionDeviceManager.EXTRA_DEVICE)
+            deviceObject?.let { viewModel.handleActivityResult(it) }
         }
     }
 
 
-    override fun onDestroy() {
-        super.onDestroy()
-        bluetoothGatt?.disconnect()
-    }
 }
